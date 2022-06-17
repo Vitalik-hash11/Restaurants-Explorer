95dcb9e8df65baae2df0150d307db3c9f34d3c85
Vitalii Karpiv <vitalii.karpiv@plus4u.net>
2022-06-17 17:02:40 +0300

    Initial Commit

---
+++ Restaurants Explorer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Restaurants Explorer
+//
+//  Created by newbie on 17.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
